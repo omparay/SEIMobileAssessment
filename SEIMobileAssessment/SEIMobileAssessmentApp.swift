2b2da320c57f92614871c9b9b45a9f07339fe27d
Oliver Paray <omparay@gmail.com>
2025-07-28 09:52:56 -0700

    Initial Commit

---
+++ SEIMobileAssessment/SEIMobileAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEIMobileAssessmentApp.swift
+//  SEIMobileAssessment
+//
+//  Created by Oliver Paray on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SEIMobileAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SEIMobileAssessmentTests/SEIMobileAssessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  SEIMobileAssessmentTests.swift
+//  SEIMobileAssessmentTests
+//
+//  Created by Oliver Paray on 7/28/25.
+//
+
+import Testing
+@testable import SEIMobileAssessment
+
+struct SEIMobileAssessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
