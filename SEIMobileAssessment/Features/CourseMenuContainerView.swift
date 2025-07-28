5857dc1a84a2c873cb53d428df91a8fe1b4aa1e7
Oliver Paray <omparay@gmail.com>
2025-07-28 10:26:22 -0700

    Changes: - Initial project layout

---
+++ SEIMobileAssessment/Features/CourseMenuContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  CourseMenuContainerView.swift
+//  SEIMobileAssessment
+//
+//  Created by Oliver Paray on 7/28/25.
+//
+
+import SwiftUI
+
+struct CourseMenuContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseMenuContainerView()
+}
---
+++ SEIMobileAssessment/Features/QuizDetailsContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizDetailsContainerView.swift
+//  SEIMobileAssessment
+//
+//  Created by Oliver Paray on 7/28/25.
+//
+
+import SwiftUI
+
+struct QuizDetailsContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizDetailsContainerView()
+}
